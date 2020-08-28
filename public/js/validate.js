d8c6e9eb1cddab21a9b5b90d51a0e786ea07f192
Esha Singh <singh.esha.dev@gmail.com>
2020-08-28 10:29:42 +0530

    Initiate Commit

---
+++ public/js/validate.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector('form');
+const location_cityName = document.querySelector('input');
+const message1 = document.getElementById('message1');
+const message2 = document.getElementById('message2');
+
+
+weatherForm.addEventListener('submit', () => {
+  event.preventDefault();
+  message1.innerHTML = '<h4>Loading...</h4>';
+  let search = location_cityName.value;
+  fetch('http://localhost:3000/weather?address=' + search).then((response) =>
+  {
+    location_cityName.value='';
+    message2.innerHTML = ' Displaying Weather Details';
+        response.json().then((data) => {
+          if (data.error){ return message1.innerHTML = data.error}
+      message1.innerHTML = 'Your City is ' + data.country + '<br> Temperature is ' + data.temperature +
+        '<br> Latitude :' + data.latitude + '<br> Longitude :' + data.longitude;
+
+    }).catch(e => message1.innerHTML =  e )
+  })
+});
+
---
+++ src/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const weather = require('./utils/weather');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+const directoryPath = path.join('__dirname', '../public');
+const viewsPath = path.join('__dirname', '../templates/views');
+const partialPath = path.join('__dirname', '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+app.use(express.static(directoryPath));
+
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+  })
+});
+
+app.get('/weather', (req, res) => {
+  //if(!req.query.address){return res.render('error404',{message: '404 Enter Address in url '})}
+  weather(req.query.address  ,(reject,data ={})=>{
+    if (reject){return res.send(reject)}
+    else {res.send(data)}
+    })
+});
+
+
+
+
+
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About' })
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help' })
+});
+// ERROR404
+app.get('/help/*', (req, res) => {
+  res.render('error404', {message: '404 /HELP/*'})
+});
+app.get('/about/*', (req, res) => {
+  res.render('error404', {message: '404 /ABOUT/*'})
+});
+app.get('/weather/*', (req, res) => {
+  res.render('error404', {message: '404 /WEATHER/*'})
+});
+app.get('*', (req, res) => {
+  res.render('error404', {message: '404 Invalid Address'})
+});
+
+
+app.listen(port, () => {
+  console.log('Server is Working')
+});
+
